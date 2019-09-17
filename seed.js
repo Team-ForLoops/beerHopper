04c1453764040cade2b4a7f81ab08ab70e41da1a
Pinzhi Zhang <pinzhi@umich.edu>
2019-09-17 11:45:02 -0500

    seedmergeworking

--- seed.js
+++ seed.js
@@ -1,6 +1,57 @@
 const Beer = require('./server/db/models/beer')
 const db = require('./server/db/db')
-// import User from '../server/db/models/user'
+const User = require('./server/db/models/user')
+
+const seedUsers = [
+  {
+    username: 'theo_truong',
+    role: 'guest',
+    email: 'theo@gmail.com',
+    password: '123theo'
+  },
+  {
+    username: 'andrea_soloko',
+    role: 'user',
+    email: 'andrea@gmail.com',
+    password: '123andrea'
+  },
+  {
+    username: 'natalie_estrada',
+    role: 'admin',
+    email: 'natalie@gmail.com',
+    password: '123natalie'
+  },
+  {
+    username: 'pinzhi_zhang',
+    role: 'guest',
+    email: 'pinzhi@gmail.com',
+    password: '123pinzhi'
+  },
+  {
+    username: 'lea_seydoux',
+    role: 'user',
+    email: 'lea@gmail.com',
+    password: '123lea'
+  },
+  {
+    username: 'jim_harbough',
+    role: 'admin',
+    email: 'jim@gmail.com',
+    password: '123jim'
+  },
+  {
+    username: 'ariana_grande',
+    role: 'user',
+    email: 'ariana@gmail.com',
+    password: '123ariana'
+  },
+  {
+    username: 'steve_green',
+    role: 'guest',
+    email: 'steve@gmail.com',
+    password: '123steve'
+  }
+]
 
 const seedBeers = [
   {
@@ -56,7 +107,7 @@ const seedBeers = [
     price: 5.99
   },
   {
-    name: 'Jalapeno Business',
+    name: 'J',
     type: 'ale',
     color: 'light',
     price: 5.0
@@ -67,22 +118,25 @@ const seedBeers = [
 const seed = async () => {
   try {
     await db.sync({force: true})
-    // const [
-    //   beerOne,
-    //   beerTwo,
-    //   beerThree,
-    //   beerFour,
-    //   beerFive,
-    //   beerSix
-    // ] =
-    await Beer.bulkCreate(seedBeers)
-    // const [
-    //   userOne,
-    //   userTwo,
-    //   userThree,
-    //   userFour,
-    //   userFive
-    // ] = await User.bulkCreate(seedUsers, {returning: true})
+    const [
+      beerOne,
+      beerTwo,
+      beerThree,
+      beerFour,
+      beerFive,
+      beerSix
+    ] = await Beer.bulkCreate(seedBeers, {returning: true})
+    const [
+      userOne,
+      userTwo,
+      userThree,
+      userFour,
+      userFive,
+      userSix,
+      userSeven,
+      userEight
+    ] = await User.bulkCreate(seedUsers, {returning: true})
+    console.log('done seeding users!')
     db.close()
   } catch (err) {
     console.log(err)
