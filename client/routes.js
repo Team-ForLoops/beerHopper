ac1b49783e28ee9d7ff33b4dcb0c0a7b5fd9d4d5
Theodore Truong <tqtruo@gmail.com>
2019-09-18 09:46:13 -0500

    Added some front-end and placeholders for adminDashboard and updateBeer

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
-
+import AdminDash from './components/adminDashboard'
 /**
  * COMPONENT
  */
@@ -21,6 +21,9 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route exact path="/beer/:beerId/edit" component={AdminDash} />{' '}
+        {/* Added placeholder */}
+        <Route exact path="/beer/dashboard" component={AdminDash} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
