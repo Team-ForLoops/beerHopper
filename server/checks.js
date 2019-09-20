4bd3e27a56d4abe7e8cb54d55bdf994d0f8e7eb5
Theodore Truong <tqtruo@gmail.com>
2019-09-19 22:58:27 -0500

    Made functions isUser and isAdmin in checks.js file to filter for users/admins

---
+++ server/checks.js
@@ -0,0 +1,23 @@
+const isUser = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    const error = new Error('You must be logged in to do that!')
+    error.status = 401
+    return next(error)
+  }
+}
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    return next()
+  } else {
+    const error = new Error(
+      'You must have Administrator privileges to do that!'
+    )
+    error.status = 401
+    return next(error)
+  }
+}
+
+module.exports = {isUser, isAdmin}
