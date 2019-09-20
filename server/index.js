dd4ebc56312f7f2a41dfe56c43d031ffa545fb24
Theodore Truong <tqtruo@gmail.com>
2019-09-19 22:47:46 -0500

    OAuth - login with google account works

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
 const {BeerOrder, Order} = require('./db/models')
+
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
